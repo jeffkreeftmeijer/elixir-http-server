111545918f780d70cd0413123d04677cd8bcce3c
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-01-16 13:58:29 +0100

    Read requests

--- lib/http.ex
+++ lib/http.ex
@@ -18,6 +18,25 @@ defmodule Http do
     accept(socket, dispatch)
   end
 
+  def read_request(request, acc \\ %{headers: []}) do
+    case :gen_tcp.recv(request, 0) do
+      {:ok, {:http_request, :GET, {:abs_path, full_path}, _}} ->
+        read_request(request, Map.put(acc, :full_path, full_path))
+
+      {:ok, :http_eoh} ->
+        acc
+
+      {:ok, {:http_header, _, key, _, value}} ->
+        read_request(
+          request,
+          Map.put(acc, :headers, [{String.downcase(to_string(key)), value} | acc.headers])
+        )
+
+      {:ok, line} ->
+        read_request(request, acc)
+    end
+  end
+
   def send_response(socket, response) do
     :gen_tcp.send(socket, response)
     :gen_tcp.close(socket)
--- lib/http/plug_adapter.ex
+++ lib/http/plug_adapter.ex
@@ -1,8 +1,13 @@
 defmodule Http.PlugAdapter do
   def dispatch(request, plug) do
+    %{headers: headers, full_path: full_path} = Http.read_request(request) |> IO.inspect()
+
     %Plug.Conn{
       adapter: {Http.PlugAdapter, request},
-      owner: self()
+      owner: self(),
+      path_info: path_info(full_path),
+      query_string: query_string(full_path),
+      req_headers: headers
     }
     |> plug.call([])
   end
@@ -22,5 +27,19 @@ defmodule Http.PlugAdapter do
     Enum.reduce(headers, "", fn {key, value}, acc ->
       acc <> key <> ": " <> value <> "\n\r"
     end)
+  end
+
+  defp path_info(full_path) do
+    [path | _] = String.split(full_path, "?")
+    path |> String.split("/") |> Enum.reject(&(&1 == ""))
+  end
+
+  defp query_string([_]), do: ""
+  defp query_string([_, query_string]), do: query_string
+
+  defp query_string(full_path) do
+    full_path
+    |> String.split("?")
+    |> query_string
   end
 end
