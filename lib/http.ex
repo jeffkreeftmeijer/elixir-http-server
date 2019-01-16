f4e67a3692b4dfacfe9c212629eb9a29f2ad85d0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-01-16 12:25:16 +0100

    Add http echo server

--- lib/http.ex
+++ lib/http.ex
@@ -1,18 +1,39 @@
 defmodule Http do
-  @moduledoc """
-  Documentation for Http.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def start_link(port: port) do
+    {:ok, socket} = :gen_tcp.listen(port, active: false, packet: :http_bin, reuseaddr: true)
+    Logger.info("Accepting connections on port #{port}")
 
-  ## Examples
+    {:ok, spawn_link(Http, :accept, [socket])}
+  end
 
-      iex> Http.hello()
-      :world
+  def accept(socket) do
+    {:ok, request} = :gen_tcp.accept(socket)
 
-  """
-  def hello do
-    :world
+    spawn(fn ->
+      body = "Hello world! The time is #{Time.to_string(Time.utc_now())}"
+
+      response = """
+      HTTP/1.1 200\r
+      Content-Type: text/html\r
+      Content-Length: #{byte_size(body)}\r
+      \r
+      #{body}
+      """
+
+      send_response(request, response)
+    end)
+
+    accept(socket)
+  end
+
+  def send_response(socket, response) do
+    :gen_tcp.send(socket, response)
+    :gen_tcp.close(socket)
+  end
+
+  def child_spec(opts) do
+    %{id: Http, start: {Http, :start_link, [opts]}}
   end
 end
--- lib/http/application.ex
+++ lib/http/application.ex
@@ -1,18 +1,12 @@
 defmodule Http.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
-
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Http.Worker.start_link(arg)
-      # {Http.Worker, arg}
+      {Http, port: 8080}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Http.Supervisor]
     Supervisor.start_link(children, opts)
   end
